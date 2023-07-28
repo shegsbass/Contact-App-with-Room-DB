2ff2483b24faa897fe34139ca0af6db1a1d9d47d
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-07-28 12:20:08 +0100

    ViewModel created

--- app/src/main/java/com/shegs/contactappwithroomdb/ContactDao.kt
+++ app/src/main/java/com/shegs/contactappwithroomdb/ContactDao.kt
@@ -13,7 +13,7 @@ interface ContactDao {
     suspend fun upsertContact(contact: Contact)
 
     @Delete
-    suspend fun deletecontact(contact: Contact)
+    suspend fun deleteContact(contact: Contact)
 
     @Query ("SELECT * FROM contact ORDER BY firstName ASC")
     fun getContactsOrderedByFirstName(): Flow<List<Contact>>

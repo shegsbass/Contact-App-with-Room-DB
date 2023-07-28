8b54f6ddbe3704accd425a60fc044d45059052ac
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-07-28 15:28:42 +0100

    Initialize Database

--- app/src/main/java/com/shegs/contactappwithroomdb/MainActivity.kt
+++ app/src/main/java/com/shegs/contactappwithroomdb/MainActivity.kt
@@ -10,13 +10,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.shegs.contactappwithroomdb.ui.ContactScreen
 import com.shegs.contactappwithroomdb.ui.theme.ContactAppWithRoomDBTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ContactDatabase::class.java,
+            "contacts.db"
+        ).build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContactAppWithRoomDBTheme {
+                ContactScreen(state = , onEvent = )
 
             }
         }

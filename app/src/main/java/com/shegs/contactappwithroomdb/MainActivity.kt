a710e566585fa30c790889eb8e9037f38d03d0c1
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-07-28 15:35:31 +0100

    Added viewModel

--- app/src/main/java/com/shegs/contactappwithroomdb/MainActivity.kt
+++ app/src/main/java/com/shegs/contactappwithroomdb/MainActivity.kt
@@ -3,13 +3,18 @@ package com.shegs.contactappwithroomdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.shegs.contactappwithroomdb.ui.ContactScreen
 import com.shegs.contactappwithroomdb.ui.theme.ContactAppWithRoomDBTheme
@@ -24,11 +29,22 @@ class MainActivity : ComponentActivity() {
         ).build()
     }
 
+    private val viewModel by viewModels<ContactViewModel> (
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ContactViewModel(db.dao) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContactAppWithRoomDBTheme {
-                ContactScreen(state = , onEvent = )
+                val state by viewModel.state.collectAsState()
+                ContactScreen(state = state, onEvent = viewModel::onEvent)
 
             }
         }
